f0ca1df0d3dbca08f2f24bc80b4d974b009c6453
0xRoS-200 <official.rohitsingh22@gmail.com>
2025-02-10 22:52:24 +0530

    feat:adding 2 questions

---
+++ Javascript/Coding Questions/For Loops/Task10.js
@@ -0,0 +1,9 @@
+/*
+Write a program to print Pascal's Triangle for a given n. For example, if n = 5, the output should look like this:
+1
+1 1
+1 2 1
+1 3 3 1
+1 4 6 4 1
+
+*/
---
+++ Javascript/Coding Questions/For Loops/Task11.js
@@ -0,0 +1,9 @@
+/*
+Write a program to print a hollow pyramid pattern for a given n. For example, if n = 5, the output should look like this:
+    *
+   * *
+  *   *
+ *     *
+*********
+
+*/
