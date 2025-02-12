16880cdcdcbb5ad42aaa5e5111537cec9e92501d
0xRoS-200 <official.rohitsingh22@gmail.com>
2025-02-12 21:30:56 +0530

    feat: solving patterns

--- Javascript/Coding Questions/For Loops/Task10.js
+++ Javascript/Coding Questions/For Loops/Task10.js
@@ -7,3 +7,25 @@ Write a program to print Pascal's Triangle for a given n. For example, if n = 5,
 1 4 6 4 1
 
 */
+
+function generatePascalsTriangle(n) {
+    let triangle = [];
+
+    for (let i = 0; i < n; i++) {
+        let row = []; // New row
+        for (let j = 0; j <= i; j++) {
+            if (j === 0 || j === i) {
+                row.push(1);
+            } else {
+                row.push(triangle[i - 1][j - 1] + triangle[i - 1][j]);
+            }
+        }
+        triangle.push(row);
+    }
+
+    for (let row of triangle) {
+        console.log(row.join(" "));
+    }
+}
+
+generatePascalsTriangle(5);
--- Javascript/Coding Questions/For Loops/Task11.js
+++ Javascript/Coding Questions/For Loops/Task11.js
@@ -7,3 +7,27 @@ Write a program to print a hollow pyramid pattern for a given n. For example, if
 *********
 
 */
+
+let n = 5;
+
+// Outer loop for rows
+for (let i = 1; i <= n; i++) {
+    let pattern_store = "";
+
+    // Printing leading spaces
+    for (let j = 1; j <= n - i; j++) {
+        pattern_store += " ";
+    }
+
+    // Printing stars and spaces for hollow part
+    for (let k = 1; k <= 2 * i - 1; k++) {
+        // Print '*' only at borders or last row
+        if (k === 1 || k === 2 * i - 1 || i === n) {
+            pattern_store += "*";
+        } else {
+            pattern_store += " ";
+        }
+    }
+
+    console.log(pattern_store);
+}
