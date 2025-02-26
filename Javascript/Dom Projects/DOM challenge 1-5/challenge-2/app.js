6043a89222d735f9fac6a41d1da933121ddb39df
0xRoS-200 <official.rohitsingh22@gmail.com>
2025-02-26 16:17:13 +0530

    feat:DOM 1 & 2 challenge solved

--- Javascript/Dom Projects/DOM challenge 1-5/challenge-2/app.js
+++ Javascript/Dom Projects/DOM challenge 1-5/challenge-2/app.js
@@ -1,3 +1,31 @@
 /**
  * Write your challenge solution here
  */
+document.addEventListener("DOMContentLoaded", () => {
+    const text = document.getElementById("mainHeading");
+    const red_button = document.getElementById("redButton");
+    const green_button = document.getElementById("greenButton");
+    const blue_button = document.getElementById("blueButton");
+    const purple_button = document.getElementById("purpleButton");
+    const reset_button = document.getElementById("resetButton");
+
+    red_button.addEventListener("click", () => {
+        text.style.color = "#e74c3c"; // Red
+    });
+
+    green_button.addEventListener("click", () => {
+        text.style.color = "#2ecc71"; // Green
+    });
+
+    blue_button.addEventListener("click", () => {
+        text.style.color = "#3498db"; // Blue
+    });
+
+    purple_button.addEventListener("click", () => {
+        text.style.color = "#9b59b6"; // Purple
+    });
+
+    reset_button.addEventListener("click", () => {
+        text.style.color = "#34495e"; // Reset to default color
+    });
+});
