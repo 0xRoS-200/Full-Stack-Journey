2ba60fcdf044b06fe37005dd3c21d02582bd0b78
0xRoS-200 <official.rohitsingh22@gmail.com>
2025-03-07 22:44:40 +0530

    feat:added responisveness

---
+++ Mini Projects/Sidebar Layout/script.js
@@ -0,0 +1,8 @@
+const dashoard_Button = document.getElementById("db-cb");
+const bookmarks_Button = document.getElementById("bm-cb");
+const Team_Button = document.getElementById("tm-cb");
+const Messages_Button = document.getElementById("msg-cb");
+const Calender_Button = document.getElementById("cal-cb");
+
+function tabSelect(){
+}
