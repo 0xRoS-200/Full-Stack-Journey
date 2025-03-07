c77ec094eecd9c384325711765c3c009afedc404
0xRoS-200 <official.rohitsingh22@gmail.com>
2025-03-08 00:46:10 +0530

    feat:added js

--- Mini Projects/Sidebar Layout/script.js
+++ Mini Projects/Sidebar Layout/script.js
@@ -1,5 +1,8 @@
-const hamburger_Button = document.getElementById("hamburger-btn");
+const buttons = document.querySelectorAll("#db-cb, #bm-cb, #tm-cb, #msg-cb, #cal-cb, #hl-cb, #se-cb");
 
-function showSidebar(){
-    
+function buttonSelect(event) {
+    buttons.forEach(button => button.classList.remove("selected-cb"));
+    event.target.classList.add("selected-cb");
 }
+
+buttons.forEach(button => button.addEventListener("click", buttonSelect));
