32e3c8a039bc65b96c7c6df23d9a882aa8af55f4
0xRoS-200 <official.rohitsingh22@gmail.com>
2025-04-10 23:32:33 +0530

    Feat: Adding database to project

--- Auth Backend Project/index.js
+++ Auth Backend Project/index.js
@@ -1,17 +1,21 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
+import db from "./utils/database.js"
 
 dotenv.config()
 
 const app = express()
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: process.env.BASE_URL,
   credentials: true,
-  method: ['GET', 'POST', 'DELETE', 'OPTIONS'], 
+  methods: ['GET', 'POST', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 const port = process.env.PORT || 5500
 
@@ -19,13 +23,16 @@ app.get('/', (req, res) => {
   res.send('Cohort!')
 })
 
-app.get('/rohit', (req, res) => {
+app.get('/rohit-singh', (req, res) => {
   res.send('Aagye Apni Maut ka Tamasha Dekhne!')
 })
 
 app.get('/divyam', (req, res) => {
   res.send('Hi Girlie!')
 })
+
+//connect to db 
+db();
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
